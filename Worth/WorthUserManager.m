1c3836102842b9cf8c26be2b5a114b3a653fe802
Patrick Butkiewicz <patrick@intrepid.io>
2015-04-03 15:17:28 -0400

    Edited celebrity compare list to match website

--- Worth/WorthUserManager.m
+++ Worth/WorthUserManager.m
@@ -69,15 +69,58 @@
 
 - (void)setupSimpleDatabase {
     NSManagedObjectContext *moc = [[WorthObjectModel sharedObjectModel] mainContext];
-    NSArray *names = @[@"Barack Obama", @"Patrick Butkiewicz", @"Aaron Tenbuuren", @"Patrick Kane", @"Patrick Ewing",
-                       @"Aaron Rodgers", @"Hank Aaron", @"Babe Ruth", @"Mario Lemieux", @"Sarah Jessica Parker",
-                       @"Brad Pitt", @"Angelina Jolie", @"Tom Wheeler", @"Larry Bird", @"Charles Barkley",
-                       @"Lebron James", @"Kobe Bryant", @"David Beckham", @"Pele", @"Macho Man Randy Savage",
-                       @"Tom Brady", @"Peyton Manning", @"Russell Wilson", @"Marshawn Lynch", @"David Lettermen",
-                       @"Jay Leno", @"Kevin"];
     
-    for (NSString *name in names) {
-        [self newWorthUserWithSalary:@(((arc4random() % 3000000) + 300000))
+    NSArray *data = @[ @"Angelina Jolie:18000000",
+                       @"Will Smith:32000000",
+                       @"Clint Eastwood:35000000",
+                       @"Tom Hanks:26000000",
+                       @"Adam Sandler:37000000",
+                       @"Madonna:125000000",
+                       @"Oprah Winfrey:82000000",
+                       @"Kate Moss:5700000",
+                       @"Sandra Bullock:14000000",
+                       @"Bill Gates:11500000000",
+                       @"Donald Trump:63000000",
+                       @"Tim Cook:9220000",
+                       @"Dick Costolo:130250",
+                       @"Mark Zuckerberg:1",
+                       @"Beyonce:115000000",
+                       @"50 Cent:8000000",
+                       @"Paul McCartney:71000000",
+                       @"Kanye West:30000000",
+                       @"Lewis Hamilton:32000000",
+                       @"Tiger Woods:61200000",
+                       @"Serena Williams:22000000",
+                       @"Wayne Rooney:23400000",
+                       @"LeBron James:64600000",
+                       @"Bode Miller:1300000",
+                       @"Average Designer:40238",
+                       @"Average Developer:66207",
+                       @"Average Head Chef:41280",
+                       @"Average Waiter:19535",
+                       @"Average Fast Food:17862",
+                       @"Average HS Teacher:45048",
+                       @"Average Dentist:123206",
+                       @"Average Engineer:64853",
+                       @"Average Barista:21252",
+                       @"Average CEO:153353",
+                       @"Average Janitor:24250",
+                       @"Average Electrician:49580",
+                       @"Average Plumber:48750",
+                       @"Average Pilot:100191",
+                       @"Sidney Crosby:12700000",
+                       @"Patrick Kane:6500000",
+                       @"Tom Brady:31300000",
+                       @"Derek Jeter:12000000",
+                       @"Kendrick Lamar:40000000",
+                       @"American Worker:26695"];
+    
+    for (NSString *person in data) {
+        NSArray *components = [person componentsSeparatedByString:@":"];
+        NSString *name = components[0];
+        NSString *salaryString = components[1];
+        NSNumber *salary = @([salaryString floatValue]);
+        [self newWorthUserWithSalary:salary
                                 name:name
                            favorited:NO
                          currentUser:NO
