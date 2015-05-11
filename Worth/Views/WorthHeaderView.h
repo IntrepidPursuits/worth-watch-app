f87f5048156e71d230e501c25ef063f7df161705
Patrick Butkiewicz <patrick@intrepid.io>
2015-05-11 11:24:33 -0400

    Added header view for information that isn't money

--- Worth/Views/WorthHeaderView.h
+++ Worth/Views/WorthHeaderView.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, WorthHeaderViewAccessoryButtonType) {
+    WorthHeaderViewAccessoryButtonTypePlus,
+    WorthHeaderViewAccessoryButtonTypeNone,
+};
+
 @interface WorthHeaderView : UIView
+
+- (void)setAccessoryType:(WorthHeaderViewAccessoryButtonType)type;
+- (void)setTitle:(NSString *)title subTitle:(NSString *)subTitle;
+- (void)setImageContainerBackgroundColor:(UIColor *)color;
+- (void)setInformationContainerBackgroundColor:(UIColor *)color;
 
 @end
--- Worth/Views/WorthHeaderView.m
+++ Worth/Views/WorthHeaderView.m
@@ -19,15 +19,16 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *subTitleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *accessoryButton;
-
+@property (nonatomic) WorthHeaderViewAccessoryButtonType accessoryButtonType;
 @end
 
 @implementation WorthHeaderView
 
 #pragma mark - Init
 
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    if ((self = [super initWithCoder:aDecoder])){
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
         self.nibView = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class])
                                                       owner:self
                                                     options:nil] lastObject];
@@ -35,7 +36,6 @@
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:nil views:@{@"bar" : self.nibView}]];
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-0-|" options:0 metrics:nil views:@{@"bar" : self.nibView}]];
         self.nibView.translatesAutoresizingMaskIntoConstraints = NO;
-        self.translatesAutoresizingMaskIntoConstraints = NO;
         [self commonInit];
     }
     return self;
@@ -48,11 +48,56 @@
 
 - (void)commonInit {
     [self.nibView setBackgroundColor:[UIColor clearColor]];
-    [self.imageContainerView setBackgroundColor:[UIColor worth_darkerGreenColor]];
-    [self.imageView setBackgroundColor:[UIColor worth_greenColor]];
-    [self.infoContainerView setBackgroundColor:[UIColor worth_darkGreenColor]];
+    [self.imageContainerView setBackgroundColor:[UIColor worth_MainPhotoAreaColor]];
+    [self.imageView setBackgroundColor:[UIColor worth_Section2PhotoColor]];
+    [self.infoContainerView setBackgroundColor:[UIColor worth_blumineColor]];
     [self.titleLabel setTextColor:[UIColor whiteColor]];
     [self.subTitleLabel setTextColor:[UIColor whiteColor]];
+}
+
+- (void)updateLayout {
+    switch (self.accessoryButtonType) {
+        case WorthHeaderViewAccessoryButtonTypeNone: {
+            self.accessoryButton.hidden = YES;
+        }
+            break;
+        case WorthHeaderViewAccessoryButtonTypePlus: {
+            self.accessoryButton.hidden = NO;
+            [self.accessoryButton setImage:[UIImage imageNamed:@"add"] forState:UIControlStateNormal];
+            [self.accessoryButton.imageView setImage:[UIImage imageNamed:@"add"]];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark - Button Events
+
+- (IBAction)accessoryButtonTapped:(id)sender {
+    NSLog(@"Tapped");
+}
+
+#pragma mark - Public
+
+- (void)setAccessoryType:(WorthHeaderViewAccessoryButtonType)type {
+    if (type != self.accessoryButtonType) {
+        _accessoryButtonType = type;
+        [self updateLayout];
+    }
+}
+
+- (void)setTitle:(NSString *)title subTitle:(NSString *)subTitle {
+    self.titleLabel.text = title;
+    self.subTitleLabel.text = subTitle;
+}
+
+- (void)setImageContainerBackgroundColor:(UIColor *)color {
+    [self.imageContainerView setBackgroundColor:color];
+}
+
+- (void)setInformationContainerBackgroundColor:(UIColor *)color {
+    [self.infoContainerView setBackgroundColor:color];
 }
 
 @end
